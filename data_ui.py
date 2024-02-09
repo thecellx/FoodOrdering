c31ecb02d722b60e6ef16944bce7c26284c0e6f5
carlol <14719472+thecellx@users.noreply.github.com>
2024-02-09 18:19:59 -0500

    16: Accept SKU with any format

--- data_ui.py
+++ data_ui.py
@@ -5,12 +5,15 @@ from menu import Menu
 
 
 class DataUI(ABC):
+    def __init__(self, food_menu: Menu):
+        self._food_menu = food_menu
+
     @abstractmethod
-    def display_food_menu(self, food_menu: Menu):
+    def display_food_menu(self):
         pass
 
     @abstractmethod
-    def display_cart(self, cart: Cart, food_menu: Menu):
+    def display_cart(self, cart: Cart):
         pass
 
     @abstractmethod
@@ -39,24 +42,29 @@ class DataUI(ABC):
 
 
 class TextDataUI(DataUI):
-    def display_food_menu(self, food_menu: Menu):
-        print('\n***** MENU *****\n')
-        for sku, name, price in food_menu:
-            # TODO The index displayed should not be a slice of the SKU. We would need some kind of mapping,
-            # or maybe food_menu should be just a list rather than a dictionary.
-            print(f"({sku[3:]}) {name}: ${price}")
 
-    def display_cart(self, cart: Cart, food_menu: Menu):
+    def __init__(self, food_menu: Menu):
+        super().__init__(food_menu)
+        # Dictionary to map SKUs to their indices. We assume the menu will never change during the execution
+        # so we only initialize this map in the constructor
+        self._sku_index_map = self._build_sku_index_map()
+
+    def display_food_menu(self):
+        print('\n***** MENU *****\n')
+        for idx, (sku, name, price) in enumerate(self._food_menu, start=1):
+            print(f"({idx}) {name}: ${price}")
+
+    def display_cart(self, cart: Cart):
         print('\n***** YOUR CART *****\n')
         # TODO alignment to be improved
         print("SKU\t\tName\t\t\tPrice\tQuantity\tTotal per item")
         subtotal = 0
         for sku, qty in cart:
-            item_name = food_menu.get_name_by_sku(sku)
-            price_per_item = food_menu.get_price_by_sku(sku)
+            item_name = self._food_menu.get_name_by_sku(sku)
+            price_per_item = self._food_menu.get_price_by_sku(sku)
             total_price_per_item = price_per_item * qty
             subtotal += (price_per_item * qty)
-            print(f"({sku[3:]})\t\t{item_name}\t\t${price_per_item}\tx\t{qty} = {total_price_per_item}")
+            print(f"({self._sku_index_map[sku]})\t\t{item_name}\t\t${price_per_item}\tx\t{qty} = {total_price_per_item}")
         taxes = round(subtotal * sales_tax, 2)
         total = round(subtotal + taxes, 2)
         print(f"Subtotal\t\t\t\t\t\t\t\t\t{subtotal}")
@@ -84,11 +92,16 @@ class TextDataUI(DataUI):
     def display_error(self, error_msg: str):
         print("ERROR: " + error_msg)
 
-    def get_sku(self):
-        print("Enter the index of the food item: ")
-        item_idx = input()
-        # TODO add some checks on item_idx
-        sku = "sku" + item_idx
+    def get_sku(self) -> str:
+        num_of_items = len(self._sku_index_map)
+        assert num_of_items > 0, "Menu appears to be empty"
+        while True:
+            print(f"Enter the index of the food item: (1 to {num_of_items})")
+            item_idx = input()
+            if item_idx.isnumeric() and int(item_idx) in range(1, len(self._sku_index_map)+1):
+                break
+            self.display_error(f"Index needs to be between 1 and {num_of_items}.")
+        sku = self._index2sku(int(item_idx))
         return sku
 
     def get_qty(self, allow_zero: bool = False):
@@ -100,3 +113,16 @@ class TextDataUI(DataUI):
                 break
             self.display_error(f"Incorrect quantity. Value needs to be a number greater than {min_qty}.")
         return int(qty)
+
+    def _build_sku_index_map(self) -> dict[str, int]:
+        sku_index_map = dict()
+        for idx, (sku, _, _) in enumerate(self._food_menu, start=1):
+            sku_index_map[sku] = idx
+        return sku_index_map
+
+    def _index2sku(self, index: int) -> str:
+        # Since dictionaries keep the order elements are added, we can assume
+        # that _sku_index_map.keys() is a list of skus ordered by index
+        index_sku_map = list(self._sku_index_map.keys())
+        sku = index_sku_map[index-1]
+        return sku
